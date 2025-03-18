bfad70c1ac75191ba982bbb6ba82d06d4e99e065
Dian Tri Budianto <dian.budianto@fastratabuana.co.id>
2025-03-19 05:58:32 +0700

    implementasi custom take pictures

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,11 +1,12 @@
 import 'dart:io';
 
+import 'package:camera/camera.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:provider/provider.dart';
-
 import '../provider/home_provider.dart';
+import 'camera_screen.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -111,7 +112,25 @@ class _HomePageState extends State<HomePage> {
     }
   }
 
-  _onCustomCameraView() async {}
+  _onCustomCameraView() async {
+    final provider = context.read<HomeProvider>();
+    final navigator = Navigator.of(context);
+
+    final cameras = await availableCameras();
+
+    final XFile? resultImageFile = await navigator.push(
+      MaterialPageRoute(
+        builder: (context) => CameraScreen(
+          cameras: cameras,
+        ),
+      ),
+    );
+
+    if (resultImageFile != null) {
+      provider.setImageFile(resultImageFile);
+      provider.setImagePath(resultImageFile.path);
+    }
+  }
 
   Widget _showImage() {
     final imagePath = context.read<HomeProvider>().imagePath;
