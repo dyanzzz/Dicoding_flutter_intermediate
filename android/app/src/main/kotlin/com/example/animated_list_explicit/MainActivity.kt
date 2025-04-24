30b7f199262aa37cad4ec9b4a0ce08280b14d1f9
dian tri budianto <dyanzzz@gmail.com>
2025-04-24 14:10:32 +0700

    add animated list explisit

---
+++ android/app/src/main/kotlin/com/example/animated_list_explicit/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.animated_list_explicit
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/login_screen.dart
@@ -0,0 +1,126 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen>
+    with SingleTickerProviderStateMixin {
+  final emailController = TextEditingController();
+
+  final passwordController = TextEditingController();
+
+  final formKey = GlobalKey<FormState>();
+
+  late final AnimationController controller;
+  late final Animation<AlignmentGeometry> animation;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 300),
+    );
+
+    animation = AlignmentTween(
+      begin: Alignment.centerLeft,
+      end: Alignment.centerRight,
+    ).animate(controller);
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    emailController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Login Screen")),
+      body: Center(
+        child: ConstrainedBox(
+          constraints: const BoxConstraints(maxWidth: 300),
+          child: Form(
+            key: formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextFormField(
+                  controller: emailController,
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  decoration: const InputDecoration(hintText: "Email"),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter your email.';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 8),
+                TextFormField(
+                  controller: passwordController,
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  obscureText: true,
+                  decoration: const InputDecoration(hintText: "Password"),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter your password.';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 8),
+                ElevatedButton(
+                  onPressed: () async {
+                    if (formKey.currentState!.validate()) {
+                      final email = emailController.text;
+                      final password = passwordController.text;
+                      final text = "Email: $email \nPassword: $password";
+                      ScaffoldMessenger.of(
+                        context,
+                      ).showSnackBar(SnackBar(content: Text(text)));
+                    }
+                  },
+                  child: const Text("LOGIN"),
+                ),
+                const SizedBox(height: 8),
+
+                AlignTransition(
+                  alignment: animation,
+                  child: ElevatedButton(
+                    onHover: (value) {
+                      if (controller.isAnimating) return;
+                      if (!formKey.currentState!.validate()) {
+                        controller.isCompleted
+                            ? controller.reverse()
+                            : controller.forward();
+                      }
+                    },
+                    onPressed: () async {
+                      if (formKey.currentState!.validate()) {
+                        final email = emailController.text;
+                        final password = passwordController.text;
+                        final text = "Email: $email \nPassword: $password";
+                        ScaffoldMessenger.of(
+                          context,
+                        ).showSnackBar(SnackBar(content: Text(text)));
+                      }
+                    },
+                    child: const Text("LOGIN"),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
