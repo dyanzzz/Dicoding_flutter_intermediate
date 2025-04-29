6a25bec32d65b7241ce64b6232593cf4dca28e12
dian tri budianto <dyanzzz@gmail.com>
2025-04-29 13:49:27 +0700

    create template with mason

---
+++ README.md
@@ -0,0 +1,19 @@
+# quote_list_app
+
+A new Flutter project with mason.
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
+A few resources to get you started if this is your first Flutter project:
+
+- dart pub global activate mason_cli (execute with console)
+- mason get (execute with console)
+- flutter create . --project-name=quote_list_app (to create folder default to running flutter)
+- update file analysis_options.yaml
+```bash
+analyzer:
+  exclude:
+    - bricks
+```
---
+++ bricks/flutter_project/__brick__/lib/screen/home_screen.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+{{#isStateless}}
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+ 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("{{appName.titleCase()}}"),
+      ),
+      body: Center(
+        child: Text(
+          "This is {{appName.titleCase()}}",
+          style: Theme.of(context).textTheme.headlineMedium,
+        ),
+      ),
+    );
+  }
+}
+{{/isStateless}}
+
+{{^isStateless}}
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("{{appName.titleCase()}}"),
+      ),
+      body: Center(
+        child: Text(
+          "This is my {{appName.titleCase()}}",
+          style: Theme.of(context).textTheme.headlineMedium,
+        ),
+      ),
+    );
+  }
+}
+{{/isStateless}}
